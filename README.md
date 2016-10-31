10a00726b84a92ee5f1c6ffa380192c8493152a0
Milos Zivojinovic <miloszivojinovic93@gmail.com>
2016-10-31 13:27:20 +0100

    new skinshot

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Link to skin: http://skins.b2evolution.net/underground-skin
 
 <img src="skinshot.png"/>
 
+#### v1.1.1
+
+- New skinshot
+
 #### v1.1.
 
 - Sitewide_header included for easier multi-blogging experience
--- _skin.class.php
+++ _skin.class.php
@@ -8,7 +8,7 @@ if( !defined('EVO_MAIN_INIT') ) die( 'Please, do not access this page directly.'
  */
 class underground_Skin extends Skin
 {
-	var $version = '1.1';
+	var $version = '1.1.1';
 	
   /**
 	 * Get default name for the skin.
